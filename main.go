8dfb9b2148bb48752039879db461ba92b8120027
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-09-19 09:10:31 +0300

    initial commit start building

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
-	
+	mux.NewRouter()
 }
---
+++ pkg/Hotel/hotels/getHotels.go
@@ -0,0 +1,7 @@
+package hotels
+
+import "net/http"
+
+func GetHotels(w http.ResponseWriter, r *http.Request) {
+	// w.Write()
+}
