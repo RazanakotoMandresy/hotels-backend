e0d35eaccbb090ee10747758ae9fd62b9f110b31
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-09-19 18:10:39 +0300

    update the main.go file place for better architecure

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 build:
-	@go build -o bin/deliveryapp-backend .
+	@go build -C cmd/app -o bin/deliveryapp-backend .
 run:build
 	@./bin/deliveryapp-backend
---
+++ cmd/app/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/RazanakotoMandresy/deliveryapp-backend/http/rest"
+)
+
+func main() {
+    if err := run(context.Background()); err != nil {
+        log.Fatalf("%+v", err)
+    }
+}
+
+func run(ctx context.Context) error {
+    server, err := rest.NewServer()
+    if err != nil {
+        return err
+    }
+    err = server.Run(ctx)
+    return err
+}
