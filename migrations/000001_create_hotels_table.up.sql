ce21bb0abf6938385b8adfd9c8c1b8dff20772d8
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-09-21 14:17:28 +0300

    better hotels tables

--- migrations/000001_create_hotels_table.up.sql
+++ migrations/000001_create_hotels_table.up.sql
@@ -1,11 +1,12 @@
 CREATE TABLE
     hotels (
-        uuid TEXT NOT NULL,
+        uuid UUID NOT NULL,
         name TEXT NOT NULL,
         description TEXT NOT NULL,
-        status SMALLINT NOT NULL,
-        created_on TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
-        updated_on TIMESTAMP(0) WITHOUT TIME ZONE,
-        deleted_on TIMESTAMP(0) WITHOUT TIME ZONE,
+        services TEXT[],
+        prix BIGINT,
+        created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+        updated_at TIMESTAMP(0) WITHOUT TIME ZONE,
+        deleted_at TIMESTAMP(0) WITHOUT TIME ZONE,
         PRIMARY KEY (uuid)
     );
