f71ce265ab5c49cb2bad546a72509e18beb9137a
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-09-25 10:22:49 +0300

    switching to id -> uuid

--- migrations/000002_create_user_table.up.sql
+++ migrations/000002_create_user_table.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users (
-        id SERIAL,
+        uuid UUID NOT NULL,
         name  TEXT NOT NULL UNIQUE,
         mail  TEXT NOT NULL UNIQUE,
         list_hotels TEXT[],
         created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
         updated_at TIMESTAMP(0) WITHOUT TIME ZONE,
         deleted_at TIMESTAMP(0) WITHOUT TIME ZONE,
-        PRIMARY KEY (id)
+        PRIMARY KEY (uuid)
     ) 
