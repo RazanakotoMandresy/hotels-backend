108b296ca8d20a749e9cedd4cfee24a12a3d7824
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-10-04 01:41:41 +0300

    better naming in the service

---
+++ internal/service/deleteHotels.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/RazanakotoMandresy/hotels-backend/middleware"
+)
+
+func (s Service) Delete(ctx context.Context, uuid string) error {
+	hotels, err := s.Get(ctx, uuid)
+	userUUID := middleware.GetUserUUIDInAuth(ctx)
+	if err != nil {
+		return err
+	}
+
+	tx, err := s.repo.Db.BeginTxx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	// Defer a rollback in case anything fails.
+	defer tx.Rollback()
+	if userUUID != hotels.CreatedBy {
+		return errors.New("you are not the creator of this hotels")
+	}
+	now := time.Now().UTC()
+	hotels.DeletedAt = &now
+	err = s.repo.Update(ctx, *hotels)
+	if err != nil {
+		return err
+	}
+
+	err = tx.Commit()
+
+	if err != nil {
+		return err
+	}
+
+	return err
+}
