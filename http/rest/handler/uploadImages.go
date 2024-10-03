583b47053a8476461c5390a88fc69168bca9b73a
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-10-04 02:04:14 +0300

    just for indexin images

---
+++ http/rest/handler/uploadImages.go
@@ -0,0 +1,8 @@
+package handler
+
+import "net/http"
+
+func (s service) UploadImages() http.HandlerFunc {
+	
+	return func(w http.ResponseWriter, r *http.Request) {}
+}
---
+++ internal/service/uploadImages.go
@@ -0,0 +1,13 @@
+package service
+
+import (
+	"context"
+
+	"github.com/RazanakotoMandresy/hotels-backend/internal/model"
+)
+
+func (s Service) UploadImages(ctx context.Context, hotelUUID string) (*model.Users, error) {
+
+	// hotels
+	return nil, nil
+}
