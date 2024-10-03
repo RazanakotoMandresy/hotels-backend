294b8d7c3b5988f1c6f5ae4cbc4bd455db962cca
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-10-04 01:36:38 +0300

    removing the warning in unused variables

--- internal/service/get.go
+++ internal/service/get.go
@@ -4,16 +4,13 @@ import (
 	"context"
 
 	"github.com/RazanakotoMandresy/hotels-backend/internal/model"
-	"github.com/RazanakotoMandresy/hotels-backend/middleware"
 )
 
-var uuids middleware.User_uuid
 
 func (s Service) Get(ctx context.Context, uuid string) (*model.Hotels, error) {
 	hotels, err := s.repo.Find(ctx, uuid)
 	if err != nil {
 		return nil, err
 	}
-
 	return hotels, nil
 }
