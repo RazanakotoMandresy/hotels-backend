4a62f82891767d2cbe846dfa5dd6dcdc2ade72a9
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-10-05 19:32:01 +0300

    can upload but a realy bad benchmark

--- internal/service/uploadImages.go
+++ internal/service/uploadImages.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"math/rand"
 
-	// "crypto/rand"
 	"io"
 	"net/http"
 	"os"
@@ -23,17 +22,18 @@ func (s Service) UploadImages(ctx context.Context, hotelUUID string, r *http.Req
 	}
 	defer file.Close()
 	splitedName := strings.Split(handler.Filename, ".")
-	newFilename := splitedName[0] + hotelUUID + fmt.Sprint(rand.Int()) + "." + splitedName[1]
-	f, err := os.OpenFile(newFilename, os.O_WRONLY|os.O_CREATE, 06666)
+	destFile := "./uploads/" + splitedName[0] + hotelUUID + fmt.Sprint(rand.Int()) + "." + splitedName[1]
+	out, err := os.Create(destFile)
 	if err != nil {
 		return "", err
 	}
-	if _, err := io.Copy(f, file); err != nil {
+	defer out.Close()
+	if _, err := io.Copy(out, file); err != nil {
 		return "", err
 	}
 	hotels.Images = append(hotels.Images, handler.Filename)
 	if err := s.repo.Update(ctx, *hotels); err != nil {
 		return "", err
 	}
-	return newFilename, nil
+	return destFile, nil
 }
