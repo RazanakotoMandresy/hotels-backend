e5164e56174443a1cca28b52d0ff199fde72b6a5
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-10-11 08:23:22 +0300

    early return if so can't upload

--- internal/service/uploadImages.go
+++ internal/service/uploadImages.go
@@ -17,7 +17,9 @@ func (s Service) UploadImages(ctx context.Context, hotelUUID string, file multip
 	if err != nil {
 		return "", err
 	}
-
+	if len(hotels.Images) > 8 {
+		return "", errors.New("8 images per hotels maximum")
+	}
 	splitedName := strings.Split(handler.Filename, ".")
 	destFile := "./uploads/" + splitedName[0] + hotelUUID + fmt.Sprint(rand.Int()) + "." + splitedName[1]
 	out, err := os.Create(destFile)
@@ -28,9 +30,7 @@ func (s Service) UploadImages(ctx context.Context, hotelUUID string, file multip
 	if _, err := io.Copy(out, file); err != nil {
 		return "", err
 	}
-	if len(hotels.Images) > 8 {
-		return "", errors.New("8 images per hotels maximum")
-	}
+
 	hotels.Images = append(hotels.Images, handler.Filename)
 	if err := s.repo.Update(ctx, *hotels); err != nil {
 		return "", err
