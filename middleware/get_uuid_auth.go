e39625a6b3e594c084710c9e5c8442b82600f190
RazanakotoMandresy <gutsshinomia@gmail.com>
2024-10-23 17:08:34 +0300

    removing the warning

--- middleware/get_uuid_auth.go
+++ middleware/get_uuid_auth.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"fmt"
 )
+var uuid User_uuid
 
 func GetUserUUIDInAuth(ctx context.Context) string {
-	var uuid User_uuid
 	uuid = "user_uuid"
 	userUUID := ctx.Value(uuid)
 	return fmt.Sprint(userUUID)
